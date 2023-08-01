936896a221869ba649ceee7e1285b2f1609b06ca
Lindsay Warr <iamalion@gmail.com>
2023-08-01 14:35:34 -0700

    Add create to course

--- UniversityRegistrar/Models/Course.cs
+++ UniversityRegistrar/Models/Course.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityRegistrar.Models
 {
     public class Course{
         public int CourseId { get; set; }
+        [Display(Name = "Course Name: ")]
         public string CourseName { get; set; }
         public List<CourseStudent> JoinEntities { get; }
     }
--- UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar/Models/Student.cs
@@ -6,9 +6,9 @@ namespace UniversityRegistrar.Models
     public class Student
     {
         public int StudentId { get; set; }
-        [Display(Name = "Student Name")]
+        [Display(Name = "Student Name: ")]
         public string StudentName { get; set; }
-        [Display(Name = "Enrollment Date")]
+        [Display(Name = "Enrollment Date: ")]
         [DataType(DataType.Date)]
         public string EnrollDate { get; set; }
         public List<CourseStudent> JoinEntities { get; }
